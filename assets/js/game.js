92084f7e3fca5209c23ec41396730ca067666106
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-09-17 13:48:59 -0400

    added start and end functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -107,30 +107,56 @@ var fight = function (enemyName) {
   }
 };
 
-//execute function
-for (var i = 0; i < enemyNames.length; i++) {
+// function to start a new game
 
-  if(playerHealth > 0){
-    window.alert ("welcome to Robot Gladiators! Round " + (i + 1));
+var startGame = function () {
+  //reset player stats
+
+  PlayerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  //execute function
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
   }
-  
-  // if you're out of health and you lose its over
-  else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
+  //play again
+  endGame();
+};
+
+// function to end the entire game
+var endGame = function () {
+  window.alert("The game has now ended. Let's see how you did!");
+
+  if (playerHealth > 0) {
+    window.alert(
+      " Great job, You've survived the game!  You now have a score of " +
+        playerMoney +
+        " . "
+    );
+  } else {
+    window.alert( "You've lost your robot in battle. ");
   }
+  //ask player if they want to play again
+  var playAgainConfirm = window.confirm(" Would you like to play again? ");
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  } else {
+    window.alert(" Thank you for playing Robot Gladiators!  Come back soon!");
+  }
+};
 
-  //pick new enemy to fight based on the index of enemy names array
-  var pickedEnemyName = enemyNames[i];
-
-  //reset enemyhealth before starting new fight
-  enemyHealth = 50;
-
-  //use debugger to pause script 
-  //debugger
-  
-  //call fight funcion with enemy robot
-  fight(pickedEnemyName);
-
-
-}
+// start the game when the page loads
+startGame();
